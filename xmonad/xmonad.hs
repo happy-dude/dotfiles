a23ecf5d64d40f3ba78c9758b8a93186fced2db5
Stanley Chan <pocketgamer5000@gmail.com>
2012-11-20 05:07:35 -0500

    Started xmonad configuration. Still very much a work in progress

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,20 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/stahn_mchan/.xmobarrc"
+    xmonad $ defaultConfig
+        { terminal = "urxvt"
+        , manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts $ layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                { ppOutput = hPutStrLn xmproc
+                , ppTitle = xmobarColor "green" "" . shorten 50
+                }
+        , normalBorderColor = "#D1D1D1"
+        , focusedBorderColor = "#856042"
+    }
