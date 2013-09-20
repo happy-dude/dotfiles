2398f003ab8df3cbf0c99d5a058ff0fa4948122d
Stanley Chan <pocketgamer5000@gmail.com>
2013-09-20 09:49:07 -0400

    Add gitgc script for repo and submodules

---
+++ gitgc.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+
+###################################################################################################
+###################################################################################################
+### Install Script for Happy-Dude's dotfiles repository
+###
+### Author: Stanley Chan
+### Github: https://github.com/Happy-Dude/dotfiles.git
+### Version: 14 April 2013
+###
+###################################################################################################
+###################################################################################################
+
+
+# Check if Git is installed update all submodules
+`which git` --version 2>&1 >/dev/null # improvement by tripleee
+GIT_IS_AVAILABLE=$?
+
+if [ $GIT_IS_AVAILABLE -eq 0 ]; then
+    GIT_PATH=`which git`
+    GIT_OPTS="submodule foreach --recursive"
+
+    echo "Running git gc"
+    $GIT_PATH gc
+    $GIT_PATH $GIT_OPTS $GIT_PATH $GIT_OPTS $GIT_PATH gc
+    $GIT_PATH $GIT_OPTS $GIT_PATH gc
+
+else
+    echo "Git not found; please get latest updates for repository submodules manually"
+fi
