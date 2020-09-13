033be5749a10f6576cafaa88cbf1b6b43ef7cc64
Stanley Chan <pocketgamer5000@gmail.com>
2020-09-12 20:28:57 -0500

    coc modules not stowable -- git doesn't like it

--- coc.nvim/.config/coc/extensions/coc-go-data/tools/pkg/mod/golang.org/x/tools@v0.0.0-20200729181040-64cdafbe085c/internal/lsp/testdata/lsp/primarymod/extract/extract_function/extract_basic.go
+++
@@ -1,7 +0,0 @@
-package extract
-
-func _() {
-	a := 1        //@mark(s0, "a")
-	var _ = 3 + 4 //@mark(e0, "4")
-	//@extractfunc(s0, e0)
-}
--- coc.nvim/.config/coc/extensions/coc-go-data/tools/pkg/mod/golang.org/x/tools@v0.0.0-20200729181040-64cdafbe085c/internal/lsp/testdata/lsp/primarymod/extract/extract_function/extract_smart_initialization.go
+++
@@ -1,9 +0,0 @@
-package extract
-
-func _() {
-	var a []int
-	a = append(a, 2) //@mark(s4, "a")
-	b := 4           //@mark(e4, "4")
-	//@extractfunc(s4, e4)
-	a = append(a, b)
-}
