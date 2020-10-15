67d4b27d29c601e5226e8e8be80816de28fa983b
Stanley Chan <pocketgamer5000@gmail.com>
2020-10-15 00:36:18 -0500

    Stowify org-mode directories
    Actual files are still in the cloud with Box.com

--- org-mode/org/.dir-locals.el
+++ org-mode/org/.dir-locals.el
@@ -1,4 +1,4 @@
-; Make org-mode, iOS BeOrg, emacs, and (Drop)Box sync play well with each other
+; Make org-mode, iOS beorg, emacs, and (Drop)Box sync play well with each other
 ; See https://christiantietze.de/posts/2019/03/sync-emacs-org-files/ and https://www.nicklanasa.com/posts/emacs-syncing
 
 ; Enable auto-revert-mode for buffers from files from this directory:
