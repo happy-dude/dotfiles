83eb8c070d27f3f57517a202f7a3a2909795f309
Stanley Chan <pocketgamer5000@gmail.com>
2021-04-01 18:37:02 -0500

    Update emacs undo-tree, include a ELPA queue.el submodule

--- emacs/.config/emacs/conf/packages/undo-tree.el
+++ emacs/.config/emacs/conf/packages/undo-tree.el
@@ -1,5 +1,7 @@
 ;; undo-tree
 
+(add-to-list 'load-path "~/.config/emacs/plugins/queue")
+(require 'queue)
 (add-to-list 'load-path "~/.config/emacs/plugins/undo-tree")
 (require 'undo-tree)
 (global-undo-tree-mode)
