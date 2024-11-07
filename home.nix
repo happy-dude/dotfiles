9320146c253cea5c0d5d422bb827a878ccaa8263
Stanley Chan <pocketgamer5000@gmail.com>
2024-11-06 23:18:50 -0600

    Manage symlinks to editorconfig and autoformatters and bat config via home-manager
    Signed-off-by: Stanley Chan <pocketgamer5000@gmail.com>

--- home.nix
+++ home.nix
@@ -50,6 +50,27 @@
       # '')
     ];
 
+    # Home Manager is pretty good at managing dotfiles. The primary way to manage
+    # plain files is through 'home.file'.
+    file = {
+      ".config/bat/config".source = ./bat/.config/bat/config;
+
+      ".clang-format".source = ./editorconfig/.clang-format;
+      ".editorconfig".source = ./editorconfig/.editorconfig;
+      ".stylua.toml".source = ./editorconfig/.stylua.toml;
+
+      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+      # # symlink to the Nix store copy.
+      # ".screenrc".source = dotfiles/screenrc;
+
+      # # You can also set the file content immediately.
+      # ".gradle/gradle.properties".text = ''
+      #   org.gradle.console=verbose
+      #   org.gradle.daemon.idletimeout=3600000
+      # '';
+    };
+
     # Home Manager can also manage your environment variables through
     # 'home.sessionVariables'. If you don't want to manage your shell through Home
     # Manager then you have to manually source 'hm-session-vars.sh' located at
@@ -67,21 +88,6 @@
     #
     sessionVariables = {
       # EDITOR = "emacs";
-    };
-
-    # Home Manager is pretty good at managing dotfiles. The primary way to manage
-    # plain files is through 'home.file'.
-    file = {
-      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-      # # symlink to the Nix store copy.
-      # ".screenrc".source = dotfiles/screenrc;
-
-      # # You can also set the file content immediately.
-      # ".gradle/gradle.properties".text = ''
-      #   org.gradle.console=verbose
-      #   org.gradle.daemon.idletimeout=3600000
-      # '';
     };
   };
 
