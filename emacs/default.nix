74f314cd74f362b47e89f04b9e3b66c29b8b2711
Stanley Chan <pocketgamer5000@gmail.com>
2024-11-07 07:14:30 -0600

    home-manager: use emacs-pgtk instead of emacs-gtk (gtk 3)
    Signed-off-by: Stanley Chan <pocketgamer5000@gmail.com>

--- emacs/default.nix
+++ emacs/default.nix
@@ -4,7 +4,7 @@
   home.file.".emacs".source = ./init.el;
   programs.emacs = {
     enable = true;
-    package = pkgs.emacs-gtk;
+    package = pkgs.emacs29-pgtk;
     extraPackages =
       epkgs: with epkgs; [
         annalist
