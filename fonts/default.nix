e28d8e2514428080a72a701b3cd12928ccb78cb7
Stanley Chan <pocketgamer5000@gmail.com>
2024-12-01 10:53:56 -0600

    home-manager: update nerd-fonts
    Signed-off-by: Stanley Chan <pocketgamer5000@gmail.com>

--- fonts/default.nix
+++ fonts/default.nix
@@ -1,9 +1,29 @@
 { config, pkgs, ... }:
 
 {
-  fonts.fontconfig.enable = true;
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      serif = [ "DejaVu Serif" ];
+      sansSerif = [ "DejaVu Sans" ];
+      monospace = [ "FiraCode Nerd Font" ];
+      emoji = [ "Noto Color Emoji" ];
+    };
+  };
+
   home.packages = with pkgs; [
+    ubuntu_font_family
+    fira
     fira-code
-    fira-code-nerdfont
+    noto-fonts
+    noto-fonts-color-emoji
+
+    nerd-fonts.comic-shanns-mono
+    nerd-fonts.dejavu-sans-mono
+    nerd-fonts.droid-sans-mono
+    nerd-fonts.fira-code
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.iosevka-term
+    nerd-fonts.noto
   ];
 }
