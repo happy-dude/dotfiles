e1a74d113fdecc1018368e1af11bf0c99fec522f
Stanley Chan <pocketgamer5000@gmail.com>
2025-01-16 19:26:12 -0600

    tmux: default-shell zsh, default-command fish
    Signed-off-by: Stanley Chan <pocketgamer5000@gmail.com>

--- tmux/default.nix
+++ tmux/default.nix
@@ -5,6 +5,12 @@
     enable = true;
     package = pkgs.tmux;
     extraConfig = ''
+      set -gu default-shell
+      set -g default-shell "${pkgs.zsh}/bin/zsh"
+
+      set -gu default-command
+      #set -g default-command "fish --login"
+
       ${builtins.readFile ./.tmux.conf}
     '';
   };
