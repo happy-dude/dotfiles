ead975092d3440e4548fda57953d484cb2b92872
Stanley Chan <pocketgamer5000@gmail.com>
2025-07-16 09:48:41 -0500

    nvim tree-sitter: org parser installed by default with nvim-orgmode
    Disable install from repo directly.
    
    Signed-off-by: Stanley Chan <pocketgamer5000@gmail.com>

--- vim/.vim/lua/treesitter.lua
+++ vim/.vim/lua/treesitter.lua
@@ -4,15 +4,15 @@ local api = vim.api
 
 require('nvim-treesitter.install').compilers = { 'gcc' }
 
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.org = {
-  install_info = {
-    url = 'https://github.com/milisims/tree-sitter-org',
-    revision = 'main',
-    files = { 'src/parser.c', 'src/scanner.c' },
-  },
-  filetype = 'org',
-}
+--local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+--parser_config.org = {
+--  install_info = {
+--    url = 'https://github.com/milisims/tree-sitter-org',
+--    revision = 'main',
+--    files = { 'src/parser.c', 'src/scanner.c' },
+--  },
+--  filetype = 'org',
+--}
 
 require('nvim-treesitter.configs').setup {
   ensure_installed = 'all', -- one of "all", "language", or a list of languages
