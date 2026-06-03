f6e6b5a9fb2bd631e947bc1a77557c9cb37cd426
Stanley Chan <pocketgamer5000@gmail.com>
2026-06-03 11:12:55 -0500

    fish: add ~/.rustowl to path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -146,6 +146,7 @@ set --universal nvm_default_version system
 #source ~/perl5/perlbrew/etc/bashrc
 # rust
 fish_add_path -p "$HOME/.cargo/bin"
+fish_add_path -p "$HOME/.rustowl"
 
 # eza
 if command -v eza &> /dev/null
